aa538be27ac2b7819ba7d02db0e1cdb129da883b
Alfonso <alfonso@mbp.local>
2022-08-29 19:26:44 -0400

    remove mongo version. add init bean

--- src/main/java/com/wiredbraincoffee/productapifunctional/ProductApiFunctionalApplication.java
+++ src/main/java/com/wiredbraincoffee/productapifunctional/ProductApiFunctionalApplication.java
@@ -1,13 +1,33 @@
 package com.wiredbraincoffee.productapifunctional;
 
+import com.wiredbraincoffee.productapifunctional.model.Product;
+import com.wiredbraincoffee.productapifunctional.repository.ProductRepository;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import reactor.core.publisher.Flux;
 
 @SpringBootApplication
 public class ProductApiFunctionalApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ProductApiFunctionalApplication.class, args);
+	}
+
+	@Bean
+	CommandLineRunner init(ProductRepository repository) {
+		return args -> {
+			Flux<Product> productFlux = Flux.just(
+							new Product(null, "Latte", 2.99),
+							new Product(null, "Latte", 2.49),
+							new Product(null, "Latte", 1.79))
+					.flatMap(repository::save);
+
+			productFlux
+					.thenMany(repository.findAll())
+					.subscribe(System.out::println);
+		};
 	}
 
 }
